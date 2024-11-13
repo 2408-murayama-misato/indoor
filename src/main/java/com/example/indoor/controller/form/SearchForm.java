7a4965324adeffaeb3094ca6301cba6d6a381c1c
tokunaga_taishu <tokunaga.taishu@alhinc.jp>
2024-11-13 16:28:44 +0900

    在庫有無検索実装

--- src/main/java/com/example/indoor/controller/form/SearchForm.java
+++ src/main/java/com/example/indoor/controller/form/SearchForm.java
@@ -1,8 +1,7 @@
 package com.example.indoor.controller.form;
 
-import com.example.indoor.Validation.MaxPrice;
-import com.example.indoor.Validation.MinPrice;
-import com.example.indoor.Validation.Number;
+
+
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,11 +12,7 @@ public class SearchForm {
     @Size(max = 30, message = "30文字以内で入力してください")
     private String keyWord;
     private String category;
-
-    @Number
-    @MinPrice
-    private Integer minPrice;
-    @Number
-    @MaxPrice
-    private Integer maxPrice;
+    private String minPrice;
+    private String maxPrice;
+    private String stockCheck;
 }
